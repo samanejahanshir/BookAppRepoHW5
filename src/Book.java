b04a1d6fd15e35dfefd5d5c7297b9b386a2dd412
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-26 17:50:45 +0330

    create class Book and add setter and getter and counstractor within class

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+public abstract class Book {
+    private  String title;
+    private  double price;
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract void setPrice(double price);
+
+
+}
