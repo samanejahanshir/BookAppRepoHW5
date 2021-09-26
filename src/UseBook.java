51cafbbbd58526a32dbc7ba24bb63480e57e0de7
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-26 18:20:24 +0330

    add UseBook class

---
+++ src/UseBook.java
@@ -0,0 +1,9 @@
+public class UseBook {
+    public static void main(String[] args) {
+        Book bookFiction=new Fiction("Sandbad");
+        Book bookNonFiction=new NonFiction("Secret");
+        System.out.println(bookFiction.toString());
+        System.out.println(bookNonFiction.toString());
+
+    }
+}
